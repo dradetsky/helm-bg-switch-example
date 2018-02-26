1022d824f655ca0111401a0af4f77b380f411890
Daniel Radetsky <dradetsky@gmail.com>
2018-02-25 23:37:08 -0800

    fix setup for flip

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@
 for chart in blue green switch ; do
     helm install -n $chart $chart
 done
+
+# simplest way to make flip.sh work; initial deploy defaults to blue
+helm upgrade --set color=green switch switch
